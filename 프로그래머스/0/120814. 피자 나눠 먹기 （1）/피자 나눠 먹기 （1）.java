8e84034bbae8512bf357119d9be745622c1bf58d
아맞다우산 <whakswp123@naver.com>
2024-05-23 23:00:10 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(n <= 7){
+            return 1;
+        }
+        if(n % 7 != 0){
+            return (n/7)+1;
+        }else{
+            answer= n/7;
+        }
+        
+        return answer;
+    }
+}
