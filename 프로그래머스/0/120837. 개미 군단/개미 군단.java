e62a1f9edcfdcc7e8c9da76f22d72329f58aa69e
아맞다우산 <whakswp123@naver.com>
2024-05-28 23:19:32 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,22 +1,5 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
-        int a = 0;
-        
-        answer = hp/5;
-        a = hp - 5*answer;
-        if(a == 0){
-            return answer;
-        }
-        
-        answer += a/3;
-        a = a - 3*(a/3);
-        if(a == 0){
-            return answer;
-        }
-        
-        answer += a/1;
-        
-        return answer;
+        return hp / 5 + (hp % 5) / 3 + (hp % 5) % 3 / 1;
     }
 }
