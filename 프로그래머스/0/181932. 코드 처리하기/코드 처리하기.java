27fb025c4f4bf56437adf6870b46d583312a1d14
아맞다우산 <whakswp123@naver.com>
2024-06-26 22:59:03 +0900

    [level 0] Title: 코드 처리하기, Time: 0.03 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(String code) {
+        StringBuilder ret = new StringBuilder();
+        int mode = 0;
+
+        for (int i = 0; i < code.length(); i++) {
+            if (mode == 0) {
+                if (i % 2 == 0 && code.charAt(i) != '1') {
+                    ret.append(code.charAt(i));
+                }
+                if (code.charAt(i) == '1') {
+                    mode = 1;
+                }
+            } else { // mode == 1
+                if (i % 2 == 1 && code.charAt(i) != '1') {
+                    ret.append(code.charAt(i));
+                }
+                if (code.charAt(i) == '1') {
+                    mode = 0;
+                }
+            }
+        }
+        if(ret.toString().equals("")){
+            return "EMPTY";
+        }
+
+        return ret.toString();
+    }
+}
