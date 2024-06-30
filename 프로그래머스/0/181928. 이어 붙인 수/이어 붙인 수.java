7f9e3da386ce3d1863f7f1ae304f728ad5c38b03
아맞다우산 <whakswp123@naver.com>
2024-07-01 00:07:37 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list) {
+        StringBuilder a = new StringBuilder();
+        StringBuilder b = new StringBuilder();
+        
+        for (int num : num_list) {
+            (num % 2 == 0 ? a : b).append(num);
+        }
+        
+        return Integer.parseInt(a.toString()) + Integer.parseInt(b.toString());
+    }
+}
