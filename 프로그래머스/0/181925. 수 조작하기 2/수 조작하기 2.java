eb8baa22d7dece6676a833e0d340c3896728f65c
Manje Cho <whakswp123@naver.com>
2024-07-01 21:54:45 +0900

    [level 0] Title: 수 조작하기 2, Time: 641.07 ms, Memory: 376 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,11 +1,15 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        
-        for(int i = 1; i < numLog.length; i++){
-            answer += numLog[i] - numLog[i-1] == 1 ? "w" : numLog[i] - numLog[i-1] == -1 ? "s" : numLog[i] - numLog[i-1] == 10 ? "d" : numLog[i] - numLog[i-1] == -10 ? "a" : "";
+        for(int i=1; i<numLog.length; i++){
+            int j = numLog[i-1] - numLog[i];
+            switch(j){
+                case -1 : answer+='w'; break;
+                case 1 : answer+='s'; break;
+                case -10 : answer+='d'; break;
+                case 10 : answer+='a'; break;
+            }
         }
-        
         return answer;
     }
 }
