98d2b245854693213e2abd42fe7444e54bace3cb
Manje Cho <whakswp123@naver.com>
2024-07-10 23:27:52 +0900

    [level 0] Title: 배열 만들기 4, Time: 5.44 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (stk.isEmpty() || stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+            } else {
+                while (!stk.isEmpty() && stk.get(stk.size() - 1) >= arr[i]) {
+                    stk.remove(stk.size() - 1);
+                }
+                stk.add(arr[i]);
+            }
+        }
+        
+        return stk.stream().mapToInt(i -> i).toArray();
+    }
+}
