1f55265a216bdae88d4aa7f49bee759f3fd39578
Manje Cho <whakswp123@naver.com>
2024-07-11 23:40:35 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = true;
-        
-        answer = x1 == false && x2 == false ? false : x1 == true && x2 == true ? x3 == true || x4 == true ? true : false : x3 == true || x4 == true ? true : false;
-        
-        return answer;
+        return (x1||x2) && (x3||x4);
     }
 }
