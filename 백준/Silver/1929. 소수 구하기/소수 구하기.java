192521b3f315dd7427a9b9434b4bcffdab31ddff
Manje Cho <whakswp123@naver.com>
2024-10-27 01:25:14 +0900

    [Silver III] Title: 소수 구하기, Time: 252 ms, Memory: 20808 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -4,6 +4,7 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
@@ -17,9 +18,8 @@ public class Main {
             }
         }
         for(int i = M; i <= N; i++){
-            if(!prime[i]){
-                System.out.println(i);
-            }
+            if(!prime[i]) sb.append(i).append('\n');
         }
+        System.out.println(sb);
 	}
 }
