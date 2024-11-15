196da7d86271e4a3232ff07877916a9d9aa833a5
Manje Cho <whakswp123@naver.com>
2024-11-15 22:25:59 +0900

    [Silver V] Title: 뒤집기, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringTokenizer st1 = new StringTokenizer(s, "0");
+        StringTokenizer st0 = new StringTokenizer(s, "1");
+        System.out.print(Math.min(st1.countTokens(), st0.countTokens()));
+    }
+}
