4746cd85bce1885fb47f01eb37d5171186224379
Manje Cho <whakswp123@naver.com>
2024-12-14 22:06:38 +0900

    [Bronze V] Title: 아스키 코드, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print((int)br.readLine().charAt(0));
+    }
+}
