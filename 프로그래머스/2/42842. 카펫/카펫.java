0e4888b380eb1e592f37d2bb62bc432e2d12eb22
Manje Cho <whakswp123@naver.com>
2025-04-01 01:39:03 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,25 +1,8 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
+        int a = (brown + 4) / 2;
+        int b = brown + yellow;
         
-        // 카펫의 전체 넓이
-        int area = brown + yellow;
-        
-        // 가능한 가로, 세로 조합 찾기
-        for (int height = 3; height <= Math.sqrt(area); height++) {
-            if (area % height == 0) {
-                int width = area / height;
-                
-                // 테두리가 갈색인 조건 확인
-                int yellowArea = (width - 2) * (height - 2);
-                if (yellowArea == yellow) {
-                    answer[0] = width;  // 가로
-                    answer[1] = height; // 세로
-                    break;
-                }
-            }
-        }
-        
-        return answer;
+        return new int[]{(int)(a + Math.sqrt(a*a - 4*b))/2,(int)(a - Math.sqrt(a*a - 4*b)) / 2};
     }
 }
