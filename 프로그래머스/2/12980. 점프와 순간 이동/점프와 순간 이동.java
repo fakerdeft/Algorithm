16058b14ecfc351a221013e8c05474a1d7c1b792
Manje Cho <whakswp123@naver.com>
2025-04-12 22:56:43 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,17 +2,6 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        while (n > 0) {
-            if (n % 2 == 1) {
-                n -= 1;
-                answer += 1;
-            } else {
-                n = n / 2;
-            }
-        }
-
-        return answer;
+        return Integer.bitCount(n);
     }
 }
