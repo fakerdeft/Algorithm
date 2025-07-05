c6208ff5d611af3bce92225edb56743c0ccb6f58
Manje Cho <whakswp123@naver.com>
2025-07-06 00:06:17 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.15 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,21 +1,20 @@
 class Solution {
     public long solution(int n) {
-           final int MOD = 1234567;
+        final int MOD = 1234567;
         
-        // n이 1이나 2인 경우 바로 반환
         if (n == 1) return 1;
         if (n == 2) return 2;
         
-        // DP 배열 생성
-        long[] dp = new long[n + 1];
-        dp[1] = 1;  // 1칸을 가는 방법: 1가지
-        dp[2] = 2;  // 2칸을 가는 방법: 2가지
+        long n1 = 1;
+        long n2 = 2;
+        long answer = 0;
         
-        // 점화식: dp[i] = dp[i-1] + dp[i-2]
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2]) % MOD;
+            answer = (n1 + n2) % MOD;
+            n1 = n2;
+            n2 = answer;
         }
-        
-        return dp[n];
+         
+        return answer;
     }
 }
