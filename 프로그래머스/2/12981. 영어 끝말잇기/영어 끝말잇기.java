16bf0feea2b6242198622d7a98e8e96a2dd63a32
Manje Cho <whakswp123@naver.com>
2025-07-07 22:55:46 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {0, 0};
+        Set<String> usedWords = new HashSet<>();
+        
+        for (int i = 0; i < words.length; i++) {
+            String currentWord = words[i];
+            
+            if (isInvalid(currentWord, words, i, usedWords)) {
+                answer[0] = (i % n) + 1;
+                answer[1] = (i / n) + 1;
+                
+                return answer;
+            }
+            
+            usedWords.add(currentWord);
+        }
+        
+        return answer;
+    }
+    
+    private boolean isInvalid(String currentWord, String[] words, int index, Set<String> usedWords) {
+        if (currentWord.length() <= 1) {
+            return true;
+        }
+        
+        if (usedWords.contains(currentWord)) {
+            return true;
+        }
+        
+        if (index > 0) {
+            String prevWord = words[index - 1];
+            char lastChar = prevWord.charAt(prevWord.length() - 1);
+            char firstChar = currentWord.charAt(0);
+            
+            if (lastChar != firstChar) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
