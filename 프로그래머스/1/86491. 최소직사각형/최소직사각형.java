aea79d3075aba442bde4a2aec3b83a083867a975
Manje Cho <whakswp123@naver.com>
2025-07-28 22:41:22 +0900

    [level 1] Title: 최소직사각형, Time: 1.93 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,22 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int[] lg = new int[sizes.length];
-        int[] sm = new int[sizes.length];
+        int width = 0;
+        int height = 0;
         
-        for(int i = 0; i < sizes.length; i++){
-            if(sizes[i][0] > sizes[i][1]){
-                lg[i] = sizes[i][0];
-                sm[i] = sizes[i][1];
-            } else {
-                lg[i] = sizes[i][1];
-                sm[i] = sizes[i][0];
-            }
+        for(int[] size : sizes){
+            width = Math.max(width, Math.max(size[0], size[1]));
+            height = Math.max(height, Math.min(size[0], size[1]));
         }
         
-        Arrays.sort(lg);
-        Arrays.sort(sm);
-        
-        return lg[sizes.length-1] * sm[sizes.length-1];
+        return width * height;
     }
 }
