171e30090d116d731ab396493947e81c74c6a91f
Manje Cho <whakswp123@naver.com>
2025-10-10 15:43:55 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.24 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,34 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        Map<String, String> map = new HashMap<>();
-        map.put("zero", "0");
-        map.put("one", "1");
-        map.put("two", "2");
-        map.put("three", "3");
-        map.put("four", "4");
-        map.put("five", "5");
-        map.put("six", "6");
-        map.put("seven", "7");
-        map.put("eight", "8");
-        map.put("nine", "9");
-        
-        String result = "";
-        String temp = "";
-        
-        for(char c : s.toCharArray()){
-            if(Character.isDigit(c)){
-                result += c;
-            } else {
-                temp += c;
-                
-                if(map.containsKey(temp)){
-                    result += map.get(temp);
-                    temp = "";
-                }
-            }
+       String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-
-        return Integer.parseInt(result);
+        return Integer.parseInt(s);
     }
 }
