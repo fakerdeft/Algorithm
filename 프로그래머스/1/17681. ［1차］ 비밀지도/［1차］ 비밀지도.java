0dda0d9cb870b78f6b5ff593ae6a4565a057d6a5
Manje Cho <whakswp123@naver.com>
2025-10-10 21:19:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 11.45 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,36 +1,17 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        String[] b1 = new String[n];
-        String[] b2 = new String[n];
-        
-        for(int i = 0; i < n; i++){
-            b1[i] = "0".repeat(n);
-            b2[i] = "0".repeat(n);
-        }
-        
-        for(int i = 0; i < n; i++){
-            String s1 = Integer.toBinaryString(arr1[i]);
-            String s2 = Integer.toBinaryString(arr2[i]);
-            
-            b1[i] = b1[i].substring(s1.length()) + s1;
-            b2[i] = b2[i].substring(s2.length()) + s2;
+        String[] result = new String[n];
+      
+        for (int i = 0; i < n; i++) {
+            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
         }
 
-        for(int i = 0; i < n; i++){
-            String temp = "";
-            
-            for(int j = 0; j < n; j++){
-                if(b1[i].charAt(j) == '1' || b2[i].charAt(j) == '1'){
-                    temp += "#";
-                } else {
-                    temp += " ";
-                }
-            }
-            
-            answer[i] = temp;
+        for (int i = 0; i < n; i++) {
+            result[i] = String.format("%" + n + "s", result[i]);
+            result[i] = result[i].replaceAll("1", "#");
+            result[i] = result[i].replaceAll("0", " ");
         }
-        
-        return answer;
+
+        return result;
     }
 }
