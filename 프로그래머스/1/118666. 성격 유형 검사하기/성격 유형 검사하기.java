1b307d50f0dcfcd845d038f442962215fc1606f6
Manje Cho <whakswp123@naver.com>
2026-01-31 00:07:59 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.90 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,17 +1,15 @@
-import java.util.*;
-import java.io.*;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
         StringBuilder sb = new StringBuilder();
-        Map<String, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         
         for(int i = 0; i < survey.length; i++){
-            String s1 = String.valueOf(survey[i].charAt(0));
-            String s2 = String.valueOf(survey[i].charAt(1));
-            int choice = choices[i];
+            char s1 = survey[i].charAt(0);
+            char s2 = survey[i].charAt(1);
             
-            switch(choice){
+            switch(choices[i]){
                 case 1: 
                     map.put(s1, map.getOrDefault(s1, 0) + 3);
                     break;
@@ -33,10 +31,10 @@ class Solution {
             }
         }
         
-        sb = map.getOrDefault("R", 0) >= map.getOrDefault("T", 0) ? sb.append("R") : sb.append("T");
-        sb = map.getOrDefault("C", 0) >= map.getOrDefault("F", 0) ? sb.append("C") : sb.append("F");
-        sb = map.getOrDefault("J", 0) >= map.getOrDefault("M", 0) ? sb.append("J") : sb.append("M");
-        sb = map.getOrDefault("A", 0) >= map.getOrDefault("N", 0) ? sb.append("A") : sb.append("N");
+        sb = map.getOrDefault('R', 0) >= map.getOrDefault('T', 0) ? sb.append("R") : sb.append("T");
+        sb = map.getOrDefault('C', 0) >= map.getOrDefault('F', 0) ? sb.append("C") : sb.append("F");
+        sb = map.getOrDefault('J', 0) >= map.getOrDefault('M', 0) ? sb.append("J") : sb.append("M");
+        sb = map.getOrDefault('A', 0) >= map.getOrDefault('N', 0) ? sb.append("A") : sb.append("N");
         
         return sb.toString();
     }
