6cc9d009d55814a0705386f05e226644f31c3a54
Manje Cho <whakswp123@naver.com>
2026-01-31 00:20:09 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 2.23 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/README.md
+++ 프로그래머스/1/118666. 성격 유형 검사하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 성격 유형 검사하기 - 118666 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666) 
 
 ### 성능 요약
 
-메모리: 92.3 MB, 시간: 0.90 ms
+메모리: 90.7 MB, 시간: 2.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 31일 00:07:56
+2026년 01월 31일 00:20:06
 
 ### 문제 설명
 
--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -2,39 +2,24 @@ import java.util.HashMap;
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        StringBuilder sb = new StringBuilder();
         HashMap<Character, Integer> map = new HashMap<>();
-        
+        int[] scores = {0, 3, 2, 1, 0, 1, 2, 3};
+
         for(int i = 0; i < survey.length; i++){
-            char s1 = survey[i].charAt(0);
-            char s2 = survey[i].charAt(1);
+            int choice = choices[i];
             
-            switch(choices[i]){
-                case 1: 
-                    map.put(s1, map.getOrDefault(s1, 0) + 3);
-                    break;
-                case 2:
-                    map.put(s1, map.getOrDefault(s1, 0) + 2);
-                    break;
-                case 3: 
-                    map.put(s1, map.getOrDefault(s1, 0) + 1);
-                    break;
-                case 5: 
-                    map.put(s2, map.getOrDefault(s2, 0) + 1);
-                    break;
-                case 6: 
-                    map.put(s2, map.getOrDefault(s2, 0) + 2);
-                    break;
-                case 7: 
-                    map.put(s2, map.getOrDefault(s2, 0) + 3);
-                    break;
-            }
+            if(choice == 4) continue;
+
+            char target = (choice < 4) ? survey[i].charAt(0) : survey[i].charAt(1);
+
+            map.put(target, map.getOrDefault(target, 0) + scores[choice]);
         }
         
-        sb = map.getOrDefault('R', 0) >= map.getOrDefault('T', 0) ? sb.append("R") : sb.append("T");
-        sb = map.getOrDefault('C', 0) >= map.getOrDefault('F', 0) ? sb.append("C") : sb.append("F");
-        sb = map.getOrDefault('J', 0) >= map.getOrDefault('M', 0) ? sb.append("J") : sb.append("M");
-        sb = map.getOrDefault('A', 0) >= map.getOrDefault('N', 0) ? sb.append("A") : sb.append("N");
+        StringBuilder sb = new StringBuilder();
+        sb.append(map.getOrDefault('R', 0) >= map.getOrDefault('T', 0) ? "R" : "T");
+        sb.append(map.getOrDefault('C', 0) >= map.getOrDefault('F', 0) ? "C" : "F");
+        sb.append(map.getOrDefault('J', 0) >= map.getOrDefault('M', 0) ? "J" : "M");
+        sb.append(map.getOrDefault('A', 0) >= map.getOrDefault('N', 0) ? "A" : "N");
         
         return sb.toString();
     }
